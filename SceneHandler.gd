459fdd26a3f63f4f65e1e28ed8813fde383edff0
Martins Bernhards <martinsdavisbernhards@gmail.com>
2022-11-27 17:53:45 +0200

    Added assets, created first map and main menu add simple logic to them

---
+++ SceneHandler.gd
@@ -0,0 +1,13 @@
+extends Node
+
+func _ready():
+	get_node("MainMenu/M/VB/NewGame").connect("pressed", self, "on_new_game_pressed")
+	get_node("MainMenu/M/VB/Quit").connect("pressed", self, "on_quit_pressed")
+	
+func on_new_game_pressed():
+	get_node("MainMenu").queue_free()
+	var game_scene = load("res://Scenes/MainScenes/GameScene.tscn").instance()
+	add_child(game_scene)
+	
+func on_quit_pressed():
+	get_tree().quit()
