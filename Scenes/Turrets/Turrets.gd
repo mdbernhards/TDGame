50f763247246f0723870fa54fb741bfc617de2a7
Martins Bernhards <martinsdavisbernhards@gmail.com>
2022-12-05 21:26:48 +0200

    Missiles shoot from the correct place on turret, when shot missiles on turret hide until reload

--- Scenes/Turrets/Turrets.gd
+++ Scenes/Turrets/Turrets.gd
@@ -53,8 +53,32 @@ func fire_gun():
 func fire_missile():
 	var new_missile = missile.instance()
 	add_child(new_missile)
+	
+	var sprite = get_node("Missile/Sprite")
+	var collisonShape = get_node("Missile/CollisionShape2D")
+	var missile_on_turret
+	
+	if left_missile:
+		missile_on_turret = get_node("Turret/Missile1")
+		left_missile = false
+		sprite.position.y -= 11
+		collisonShape.position.y -= 11
+		print(position)
+	else:
+		missile_on_turret = get_node("Turret/Missile2")
+		left_missile = true
+		sprite.position.y += 11
+		collisonShape.position.y += 11
+		print(position)
+		
+	sprite.position.x += 3
+	collisonShape.position.x += 3
+	
+	missile_on_turret.hide()
 	new_missile.start(get_node("Turret").position, Vector2(1,0).rotated(get_node("Turret").global_rotation))
-
+	yield(get_tree().create_timer(GameData.tower_data["MissileT1"]["rof"]/ 1.5), "timeout")
+	missile_on_turret.show()
+	
 func _on_Range_body_entered(body):
 	enemy_array.append(body.get_parent())
 	print(enemy_array)
